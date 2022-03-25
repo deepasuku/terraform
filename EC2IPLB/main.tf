b2afdbc5e328afb7a2049fe24bdf8b27c27c4305
deepa <98995513+deepas745@users.noreply.github.com>
2022-03-25 08:32:25 -0400

    terraform code

--- EC2IPLB/main.tf
+++ EC2IPLB/main.tf
@@ -2,14 +2,31 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-
 resource "aws_instance" "temp1" {
     ami = "ami-0c02fb55956c7d316"
     instance_type = "t2.micro"
     associate_public_ip_address =  true
 
+  user_data = <<-EOF
+  #!/bin/bash -xe
+	yum intall httpd -y
+	systemctl start httpd
+  EOF
   tags = {
     Name = "devwebInstance"
   }
 }
+resource "aws_instance" "temp2" {
+    ami = "ami-0c02fb55956c7d316"
+    instance_type = "t2.micro"
+    associate_public_ip_address =  true
 
+  user_data = <<-EOF
+  #!/bin/bash -xe
+	yum intall httpd -y
+	systemctl start httpd
+  EOF
+  tags = {
+    Name = "prodwebInstance"
+  }
+}
